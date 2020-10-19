00bdba9e95dc94129e87ab7374d66587ae056b60
Appdev33 <Appdev33@gmail.com>
2020-10-19 16:33:39 +0530

    Regrouping Folders

---
+++ PROJECT_1/Assign.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+Url = "https://api.macaddress.io/v1?apiKey=at_Xv2Z6uVSr0a5O045KtgDw2F0Ri5gQ&output=json&search=44:38:39:ff:ef:57"
+payload= {}
+headers= {}
+
+response = requests.request("GET", Url, headers=headers, data = payload)
+status_code = response.status_code
+content = json.loads(response.content)
+#print(status_code)
+#print (json.dumps(content, indent=2))
+print(content['vendorDetails']['companyName'])
