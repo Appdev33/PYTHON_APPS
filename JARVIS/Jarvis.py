2ab73459bc25ec08c9d75620d3df176204fda3c9
appdev33 <app.devplatform@gmail.com>
2021-06-09 17:25:00 +0530

    Changes to Jarvis on Response Reply

--- JARVIS/Jarvis.py
+++ JARVIS/Jarvis.py
@@ -7,28 +7,106 @@ engine = pyttsx3.init()
 
 recognizer = sr.Recognizer()
 
-''' recording the sound '''
-while(True):
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def Record(*argv):
     with sr.Microphone() as source:
-        print("Adjusting noise ")
-        #recognizer.adjust_for_ambient_noise(source, duration=1)
-        print("Recording for 4 seconds")
-        recorded_audio = recognizer.listen(source, timeout=4)
-        print("Done recording")
+        if(argv[0]!= None):
+            speak(argv[0])
+            print("Adjusting noise ")
+            # recognizer.adjust_for_ambient_noise(source, duration=1)
+            print("Recording for 4 seconds")
+            recorded_audio = recognizer.listen(source, timeout=5)
+
+            print(type(recorded_audio))
+            print("Done recording")
+        else:
+            print("Taking Reply")
+            recorded_audio = recognizer.listen(source, timeout=3)
 
 
-    try:
-        print("Recognizing the text")
-        text = recognizer.recognize_google(
-                recorded_audio,
+    return recorded_audio
+
+''' recording the sound '''
+def Command():
+    while(True):
+
+
+
+        try:
+
+            Rec = Record("Hello Sir! How can I Help You!")
+            print("Recognizing the text")
+            text = recognizer.recognize_google(
+                    Rec,
+                    language="en-US"
+                )
+            print("Decoded Text : {}".format(text))
+            time.sleep(2)
+
+            speak("Do you Mean to say ?")
+            speak(text)
+            time.sleep(1)
+            Reply = Record()
+            print('hello')
+            text = recognizer.recognize_google(
+                Reply,
                 language="en-US"
             )
-        print("Decoded Text : {}".format(text))
-        time.sleep(2)
-        if(text=="hello"):
-            engine.say("Hello Sir! How can I Help You!")
-            engine.runAndWait()
+            print(Reply)
+            time.sleep(2)
+
+            #
+            if(Reply=="Yes"):
+                speak("Searching what you said")
 
 
-    except Exception as ex:
-        print(ex)
+
+
+        except Exception as ex:
+            print(ex)
+            speak("Unable to Recognize your voice. Please try Again After beep")
+
+
+
+
+# Additional Functionalities
+
+Command()
+
+def Time():
+    pass
+def Search():
+    pass
+def Email():
+    pass
+def Reminder():
+    pass
+def Bookings():
+    pass
+def TrackPrices():
+    pass
+def LocateThings():
+    pass
+def Call():
+    pass
+def SendMsg():
+    pass
+def SetAlarm():
+    pass
+def Weather():
+    pass
+def News():
+    pass
+def PlayMusic():
+    pass
+def PlayVideo():
+    pass
+def WhoisThere():
+    pass
+
+
