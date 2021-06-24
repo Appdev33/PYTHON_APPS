21343f53e1346af0a3d8734bec3328144674b58d
appdev33 <app.devplatform@gmail.com>
2021-06-24 20:02:08 +0530

    Changes to update modularise code and add Myntra Skeleton

--- Automatic Birthday Wisher/BdayWisher.py
+++ Automatic Birthday Wisher/BdayWisher.py
@@ -1,14 +1,30 @@
 import json
-import datetime
+# import datetime
 from datetime import date
-import telebot
-import telethon
+# import telebot
+# import telethon
+import pywhatkit
 
 
 today = date.today()
 #open the file
 f = open('data.json',"r")
 data = json.load(f)
+
+try:
+
+    # sending message to reciever
+    # using pywhatkit
+    pywhatkit.sendwhatmsg("+919899480737",
+                          "Hello from GeeksforGeeks",
+                          22, 28)
+    print("Successfully Sent!")
+
+except:
+
+    # handling exception
+    # and printing error message
+    print("An Unexpected Error!")
 
 for i in data['Birthday_Details']:
     Cur = today.strftime("%d-%m")
